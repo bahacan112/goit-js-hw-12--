69d3262597dbb6391319f9169bfb881142ad9a3e
bahacan112 <bzenbil19@gmail.com>
2024-09-09 07:37:48 +0300

    sabah güncellemesi

--- src/main.js
+++ src/main.js
@@ -1,74 +1,124 @@
+import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import 'iziToast/dist/css/iziToast.min.css'; 
+import 'iziToast/dist/css/iziToast.min.css';
 import iziToast from 'izitoast';
+
+class ImageGallery {
+  constructor(searchInput, searchButton, gallery, loadingSpinner, loadMoreButton) {
+    this.searchInput = searchInput;
+    this.searchButton = searchButton;
+    this.gallery = gallery;
+    this.loadingSpinner = loadingSpinner;
+    this.loadMoreButton = loadMoreButton;
+    this.currentPage = 1;
+    this.currentQuery = '';
+    this.lightbox = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionsData: 'alt',
+      captionDelay: 250
+    });
+
+    this.initialize();
+  }
+
+  initialize() {
+    this.loadMoreButton.classList.add('hidden')
+
+    this.searchButton.addEventListener('click', this.handleSearch.bind(this));
+    this.loadMoreButton.addEventListener('click', this.loadMoreImages.bind(this));
+  }
+
+  async handleSearch() {
+    this.currentQuery = this.searchInput.value;
+    this.currentPage = 1;
+    this.gallery.innerHTML = ''; 
+    await this.fetchImages(); 
+  }
+
+  async loadMoreImages() {
+    this.currentPage++; 
+
+    await this.fetchImages(); 
+    this.scrollGallery();
+  }
+  scrollGallery() {
+    const galleryItems = this.gallery.querySelectorAll('.gallery-item');
+    if (galleryItems.length > 1) {
+      const itemHeight = galleryItems[0].getBoundingClientRect().height;
+      window.scrollBy({ top: itemHeight * 2, behavior: 'smooth' });
+    }
+  }
+  async fetchImages() {
+    const url = `${import.meta.env.VITE_API_URL}/?key=${import.meta.env.VITE_API_KEY}&q=${this.currentQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.currentPage}&page_per=40`;
+
+    this.loadingSpinner.classList.remove('hidden');
+    this.loadMoreButton.classList.remove('hidden')
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      console.log(data)
+      const galleryItems = data.hits
+      .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+          <li class="gallery-item">
+            <a href="${largeImageURL}" data-lightbox="gallery" data-title="${tags}">
+              <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
+            </a>
+            <div class="bottom-item">
+              <div class="bottom-item-div"><p class="bottom-item-p1">Likes</p><p class="bottom-item-p2">${likes}</p></div>
+              <div class="bottom-item-div"><p class="bottom-item-p1">Views</p><p class="bottom-item-p2">${views}</p></div>
+              <div class="bottom-item-div"><p class="bottom-item-p1">Comments</p><p class="bottom-item-p2">${comments}</p></div>
+              <div class="bottom-item-div"><p class="bottom-item-p1">Downloads</p><p class="bottom-item-p2">${downloads}</p></div>
+            </div>
+          </li>
+        `;
+      })
+      .join("");
+      this.gallery.innerHTML += galleryItems;
+
+     
+      if (data.hits.length===0) {
+        iziToast.info({
+          title: 'Info',
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+          backgroundColor: 'red',
+        });
+        this.loadingSpinner.classList.add('hidden');
+        this.loadMoreButton.classList.add('hidden')
+        return;
+      }
+console.log(data.totalHits-(this.currentPage*40))
+
+      if (data.totalHits-(this.currentPage*40) < 40 && this.currentPage >= 1) {
+        this.loadMoreButton.classList.add('hidden'); 
+        this.loadingSpinner.classList.add('hidden');
+
+        iziToast.info({
+          title: 'Info',
+          message: `We're sorry, but you've reached the end of search results`,
+          position: 'topRight',
+          backgroundColor: 'yellow',
+        });
+        return;
+      }
+
+
+      this.loadMoreButton.classList.remove()
+      this.lightbox.refresh(); 
+    } catch (error) {
+      console.error('Hata:', error);
+    } finally {
+      this.loadingSpinner.classList.add('hidden'); 
+    }
+  }
+}
 
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const gallery = document.getElementById('gallery');
 const loadingSpinner = document.getElementById('loading-spinner');
+const loadMoreButton = document.getElementById('load-more-button');
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionsData: 'alt',
-  captionDelay: 250
-});
-
-searchButton.addEventListener('click', () => {
-  const url = `${import.meta.env.VITE_API_URL}/?key=${import.meta.env.VITE_API_KEY}&q=${searchInput.value}&image_type=photo&orientation=horizontal&safesearch=true`;
-  loadingSpinner.classList.remove('hidden');
-
-  const fetchData = (url) => {
-    return new Promise((resolve, reject) => {
-      fetch(url)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Veri alınırken bir hata oluştu');
-          }
-          return response.json();
-        })
-        .then((data) => resolve(data))
-        .catch((error) => reject('Bir hata oluştu: ' + error.message));
-    });
-  };
-
-  fetchData(url)
-    .then((data) => {
-        if (data.hits.length===0){
-            iziToast.info({
-                title: 'Info',
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-                position: 'topRight',
-                backgroundColor: 'red',
-              });
-        }
-      const galleryItems = data.hits
-        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-          return `
-            <li class="gallery-item">
-              <a href="${largeImageURL}" data-lightbox="gallery" data-title="${tags}">
-                <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
-              </a>
-              <div class="bottom-item">
-                <div class="bottom-item-div"><p class="bottom-item-p1">Likes</p><p class="bottom-item-p2">${likes}</p></div>
-                <div class="bottom-item-div"><p class="bottom-item-p1">Views</p><p class="bottom-item-p2">${views}</p></div>
-                <div class="bottom-item-div"><p class="bottom-item-p1">Comments</p><p class="bottom-item-p2">${comments}</p></div>
-                <div class="bottom-item-div"><p class="bottom-item-p1">Downloads</p><p class="bottom-item-p2">${downloads}</p></div>
-              </div>
-            </li>
-          `;
-        })
-        .join("");
-    
-
-      gallery.innerHTML = galleryItems;
-      lightbox.refresh();
-    })
-    .catch((error) => {
-      console.log('Hata:', error);
-    })
-    .finally(() => {
-      loadingSpinner.classList.add('hidden');
-      
-    });
-});
+const imageGallery = new ImageGallery(searchInput, searchButton, gallery, loadingSpinner, loadMoreButton);
